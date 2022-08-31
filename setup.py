33c9f74755a488d3a6af0cb9802c759f5afa9794
17TheWord <54731914+17TheWord@users.noreply.github.com>
2022-09-01 00:46:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="nonebot-plugin-mcqq-mcrcon",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
-    version="0.0.3",  # 程序版本
+    name="nonebot-plugin-mcqq",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="0.0.8",  # 程序版本
     author="17TheWord",  # 项目作者
     author_email="17theword@gmail.com",  # 作者邮件
     description="基于NoneBot的QQ群聊与Minecraft Server消息互通插件",  # 项目的一句话描述
     long_description=long_description,  # 加长版描述？
     long_description_content_type="text/markdown",  # 描述使用Markdown
-    url="https://github.com/17TheWord/nonebot-plugin-mcqq/tree/mcqq_mcrcon",  # 项目地址
+    url="https://github.com/17TheWord/nonebot-plugin-mcqq",  # 项目地址
     packages=setuptools.find_packages(),  # 无需修改
     classifiers=[
         "Programming Language :: Python :: 3.10",  # 使用Python3.10
@@ -23,6 +23,5 @@ setuptools.setup(
         'nonebot-adapter-onebot>=2.1.1',
         'nonebot-plugin-guild-patch>=0.2.0',
         'websockets>=10.3',
-        'mcrcon>=0.7.0',
     ],
 )
