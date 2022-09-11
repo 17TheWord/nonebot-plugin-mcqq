af42fe1cf752731a2e8400067c8fbba2f04160d3
17TheWord <17theword@gmail.com>
2022-09-12 02:44:04 +0800

    新增插件名、群昵称颜色

--- nonebot_plugin_mcqq_mcrcon/__init__.py
+++ nonebot_plugin_mcqq_mcrcon/__init__.py
@@ -3,7 +3,8 @@ from nonebot import on_message
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 
 from nonebot_plugin_guild_patch import GuildMessageEvent
-from .data_source import send_msg_to_mc, on_mcrcon_connect, on_connect, mcr, group_list
+from .data_source import send_msg_to_mc, on_mcrcon_connect, on_connect, dis_mcrcon_connect
+from .utils import get_mc_qq_group_list, get_mc_qq_guild_list
 
 mc_qq_mcrcon = on_message(priority=5)
 
@@ -12,9 +13,9 @@ driver = get_driver()
 
 # Bot 连接时
 @driver.on_bot_connect
-async def do_something(bot: Bot):
+async def on_start(bot: Bot):
     while True:
-        await on_mcrcon_connect()
+        on_mcrcon_connect(bot=bot)
         await on_connect(bot=bot)
         if not OSError:
             break
@@ -22,17 +23,17 @@ async def do_something(bot: Bot):
 
 # 断开连接时
 @driver.on_shutdown
-async def do_something():
-    mcr.disconnect()
+async def on_stop():
+    dis_mcrcon_connect()
 
 
 # 收到 群/频 道消息时
 @mc_qq_mcrcon.handle()
 async def handle_first_receive(bot: Bot, event: GuildMessageEvent | GroupMessageEvent):
     if isinstance(event, GroupMessageEvent):
-        if event.group_id in group_list['group_list']:
+        if event.group_id in get_mc_qq_group_list(bot=bot):
             await send_msg_to_mc(bot=bot, event=event)
     elif isinstance(event, GuildMessageEvent):
-        for per_channel in group_list['guild_list']:
-            if event.guild_id == per_channel['guild_id'] and event.channel_id == per_channel['channel_id']:
+        for per_channel in get_mc_qq_guild_list(bot=bot):
+            if event.guild_id == per_channel[0] and event.channel_id == per_channel[1]:
                 await send_msg_to_mc(bot=bot, event=event)
--- nonebot_plugin_mcqq_mcrcon/data_source.py
+++ nonebot_plugin_mcqq_mcrcon/data_source.py
@@ -1,23 +1,19 @@
+import time
 import mcrcon
 import websockets
 import nonebot
 from mcrcon import MCRcon
+from .utils import *
 
-from nonebot.adapters.onebot.v11 import GroupMessageEvent
-from nonebot_plugin_guild_patch import GuildMessageEvent
+global mcr
 
-from src.mc_qq_config import group_list, mc_ip, ws_port, mcrcon_password, mcrcon_port
 
 # 配置 MCRcon 连接信息
-# TODO 从 .env.* 配置文件获取连接信息
-mcr = MCRcon(mc_ip, mcrcon_password, mcrcon_port)
-
-
 async def on_connect(bot):
     # 全局变量 websocket
     global websocket
 
-    url = f"ws://{mc_ip}:{ws_port}"
+    url = f"ws://{get_mc_qq_ip(bot=bot)}:{get_mc_qq_ws_port(bot=bot)}"
 
     # 建立链接
     try:
@@ -26,99 +22,36 @@ async def on_connect(bot):
             while True:
                 # 后台日志
                 # 接收消息赋值
-                get_msg = await websocket.recv()
-
-                if group_list['group_list']:
-                    for per_group in group_list['group_list']:
-                        await bot.call_api("send_group_msg", group_id=per_group, message=get_msg)
-                if group_list['guild_list']:
-                    for per_guild in group_list['guild_list']:
-                        await bot.call_api("send_guild_channel_msg", guild_id=per_guild['guild_id'],
-                                           channel_id=per_guild['channel_id'], message=get_msg)
-                nonebot.logger.success("[MC_QQ]丨发送消息：" + get_msg)
+                recv_msg = await websocket.recv()
+                # 发送消息到 QQ
+                await send_msg_to_qq(bot=bot, recv_msg=recv_msg)
+                nonebot.logger.success("[MC_QQ]丨发送消息：" + recv_msg)
     except (OSError, websockets.exceptions.ConnectionClosedError, websockets.exceptions.ConnectionClosedOK):
         nonebot.logger.error("[MC_QQ]丨无法连接到 MC_QQ WebSocket 服务器，正在重新连接。")
         await on_connect(bot=bot)
 
 
-async def on_mcrcon_connect():
+def on_mcrcon_connect(bot: Bot):
     try:
+        mcr = MCRcon(get_mc_qq_ip(bot=bot), get_mc_qq_mcrcon_password(bot=bot), get_mc_qq_mcrcon_port(bot=bot))
         mcr.connect()
     except (OSError, ConnectionRefusedError):
         nonebot.logger.error("[MC_QQ_Rcon]丨无法连接到 MCRcon，正在重新连接。")
-        await on_mcrcon_connect()
+        time.sleep(3)
+        on_mcrcon_connect(bot=bot)
+
+
+def dis_mcrcon_connect():
+    mcr.disconnect()
 
 
 # 发送消息到 Minecraft
-async def send_msg_to_mc(bot, event):
-    # 命令信息起始
-    command_msg = 'tellraw @a ["",'
-    # 插件名与发言人昵称
-    command_msg += '{"text": "[MC_QQ] ' + event.sender.nickname + '说：","color": "white"},'
-    # 文本信息
-    text_msg = ''
-    for msg in event.message:
-        # 文本
-        if msg.type == "text":
-            command_msg += '{"text": "' + msg.data['text'].replace("\r\n", " ") + ' ","color": "white"},'
-            text_msg += msg.data['text'].replace("\r\n", " ")
-        # 图片
-        elif msg.type == "image":
-            command_msg += '{"text": "[图片] ","color": "yellow","clickEvent": {"action": "open_url","value": "' + \
-                           msg.data[
-                               'url'] + '"},"hoverEvent": {"action": "show_text","contents": [{"text": "查看图片","color": "dark_purple"}]}},'
-            text_msg += '[图片] '
-        # 表情
-        elif msg.type == "face":
-            command_msg += '{"text": "[表情]","color": "white"},'
-            text_msg += '[表情] '
-        # 语音
-        elif msg.type == "record":
-            command_msg += '{"text": "[语音]","color": "white"},'
-            text_msg += '[语音] '
-        # 视频
-        elif msg.type == "video":
-            command_msg += '{"text": "[视频] ","color": "yellow","clickEvent": {"action": "open_url","value": "' + \
-                           msg.data[
-                               'url'] + '"},"hoverEvent": {"action": "show_text","contents": [{"text": "查看视频","color": "dark_purple"}]}},'
-            text_msg += '[视频] '
-        # @
-        elif msg.type == "at":
-            # 获取 群/频道 昵称
-            member_nickname = await get_member_nickname(bot, event, msg.data['qq'])
-            command_msg += '{"text": "@' + member_nickname + ' ","color": "aqua"},'
-            text_msg += '@' + member_nickname + ' '
-        # share
-        elif msg.type == "share":
-            command_msg += '{"text": "[分享：' + msg.data[
-                'title'] + '] ","color": "yellow","clickEvent": {"action": "open_url","value": "' + msg.data[
-                               'url'] + '"},"hoverEvent": {"action": "show_text","contents": [{"text": "查看图片","color": "dark_purple"}]}},'
-            text_msg += '[分享：' + msg.data['title'] + '] '
-        # forward
-        elif msg.type == "forward":
-            # 获取合并转发 await bot.get_forward_msg(message_id=event.message_id)
-            command_msg += '{"text": "[合并转发] ","color": "white"},'
-            text_msg += '[合并转发] '
-        else:
-            command_msg += '{"text": "[ ' + msg.type + '] ","color": "white"},'
-            text_msg += '[' + msg.type + '] '
-    command_msg += ']'
-    command_msg = command_msg.replace("},]", "}]")
+async def send_msg_to_mc(bot: Bot, event):
+    text_msg, command_msg = await msg_process(bot=bot, event=event)
     try:
         mcr.command(command_msg)
     except (mcrcon.MCRconException, ConnectionResetError, ConnectionAbortedError):
         nonebot.logger.error("[MC_QQ_Rcon]丨无法发送消息，MCRcon 未连接。")
-        await on_mcrcon_connect()
+        on_mcrcon_connect(bot=bot)
         mcr.command(command_msg)
     nonebot.logger.success("[MC_QQ]丨发送消息：" + text_msg)
-
-
-# 获取昵称
-async def get_member_nickname(bot, event, user_id):
-    # 判断从 群 或者 频道 获取成员信息
-    if isinstance(event, GroupMessageEvent):
-        member_info = await bot.get_group_member_info(group_id=event.group_id, user_id=user_id)
-    elif isinstance(event, GuildMessageEvent):
-        await bot.get_guild_member_profile(guild_id=event.guild_id, user_id=str(user_id))
-    # 返回成员昵称
-    return member_info['nickname']
