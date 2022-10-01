7050d47bebb137fb5f6c3c4fb4ca3c8245017f37
17TheWord <17theword@gmail.com>
2022-10-01 23:54:06 +0800

    修复TypeError的问题

--- nonebot_plugin_mcqq/__init__.py
+++ nonebot_plugin_mcqq/__init__.py
@@ -23,10 +23,10 @@ async def on_start(bot: Bot):
 # 收到 群/频 道消息时
 @mc_qq.handle()
 async def handle_first_receive(bot: Bot, event: GroupMessageEvent | GuildMessageEvent):
-    if isinstance(event, GroupMessageEvent):
+    if event.message_type == "group":
         if event.group_id in get_mc_qq_group_list(bot=bot):
             await send_msg_to_mc(bot=bot, event=event)
-    elif isinstance(event, GuildMessageEvent):
+    elif event.message_type == "guild":
         for per_channel in get_mc_qq_guild_list(bot=bot):
             if event.guild_id == per_channel[0] and event.channel_id == per_channel[1]:
                 await send_msg_to_mc(bot=bot, event=event)
