a46cb0d7186d3fc52623a97dca8637be39b8dcfb
17TheWord <17theword@gmail.com>
2023-01-04 00:34:04 +0800

    minecraft_server重构

---
+++ minecraft_server/src/com/scareye/mcqq/WSClient.java
@@ -0,0 +1,92 @@
+package com.scareye.mcqq;
+
+import com.alibaba.fastjson.JSONObject;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static com.scareye.mcqq.MC_QQ.wsClient;
+import static com.scareye.mcqq.MC_QQ.connectTime;
+import static com.scareye.mcqq.ConfigReader.config;
+import static com.scareye.mcqq.Utils.say;
+
+public class WSClient extends WebSocketClient {
+
+
+    public WSClient() throws URISyntaxException {
+        super(new URI("ws://" + config().get("websocket_hostname") + ":" + config().get("websocket_port")));
+    }
+
+    /**
+     * 连接打开时
+     *
+     * @param serverHandshake ServerHandshake
+     */
+    @Override
+    public void onOpen(ServerHandshake serverHandshake) {
+        JSONObject connectEvent = new JSONObject();
+        connectEvent.put("server_name", config().get("server_name"));
+        connectEvent.put("event_name", "ConnectEvent");
+        connectEvent.put("status", wsClient.isOpen());
+        sendMessage(connectEvent.toJSONString());
+        connectTime = 0;
+        say("已成功连接 WebSocket 服务器。");
+    }
+
+    /**
+     * 收到消息时触发
+     * 向服务器游戏内公屏发送信息
+     */
+    @Override
+    public void onMessage(String message) {
+    }
+
+    /**
+     * 关闭时
+     *
+     * @param i 关闭码
+     * @param s 关闭信息
+     * @param b 是否关闭
+     */
+    @Override
+    public void onClose(int i, String s, boolean b) {
+        if (wsClient != null) {
+            wsClient.sendPing();
+        }
+    }
+
+    /**
+     * 触发异常时
+     *
+     * @param exception 所有异常
+     */
+    @Override
+    public void onError(Exception exception) {
+        if (wsClient != null) {
+            connectTime++;
+            say("WebSocket 连接已断开,正在第 " + connectTime + " 次重新连接。");
+            try {
+                wsClient = new WSClient();
+                Thread.sleep(3000);
+                wsClient.connectBlocking();
+            } catch (URISyntaxException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param message 消息
+     */
+    public void sendMessage(String message) {
+        if (wsClient.isOpen()) {
+            wsClient.send(message);
+        } else {
+            say("发送消息失败，没有连接到 WebSocket 服务器。");
+        }
+    }
+}
