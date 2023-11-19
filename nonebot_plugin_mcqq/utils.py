75510b9831bafd920a36f63d75804f7443a340a5
17TheWord <17theword@gmail.com>
2023-11-19 22:22:15 +0800

    适配QQ适配器频道

--- nonebot_plugin_mcqq/utils.py
+++ nonebot_plugin_mcqq/utils.py
@@ -1,6 +1,7 @@
 from typing import Union
 from nonebot import get_driver
 from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot.adapters.qq import MessageCreateEvent
 from nonebot_plugin_guild_patch import GuildMessageEvent
 
 from mcqq_tool.config import Config
@@ -9,19 +10,19 @@ plugin_config: Config = Config.parse_obj(get_driver().config)
 
 rule_guild_list = []
 rule_group_list = []
-for per_server in plugin_config.mc_qq_server_list:
+for per_server in plugin_config.mc_qq_server_dict.values():
     if guild_list := per_server.guild_list:
         for per_guild in guild_list:
             rule_guild_list.append(f"{per_guild.guild_id}:{per_guild.channel_id}")
     if group_list := per_server.group_list:
         for per_group in group_list:
-            rule_group_list.append(per_group)
+            rule_group_list.append(per_group.group_id)
 
 
-async def msg_rule(event: Union[GroupMessageEvent, GuildMessageEvent]) -> bool:
+async def msg_rule(event: Union[GroupMessageEvent, GuildMessageEvent, MessageCreateEvent]) -> bool:
     """Rule 消息规则"""
     if isinstance(event, GroupMessageEvent):
         return event.group_id in rule_group_list
-    elif isinstance(event, GuildMessageEvent):
+    elif isinstance(event, GuildMessageEvent | MessageCreateEvent):
         return f"{event.guild_id}:{event.channel_id}" in rule_guild_list
     return False
