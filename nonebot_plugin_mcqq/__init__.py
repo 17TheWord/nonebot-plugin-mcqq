98da24ccb0211a05dadcf59afc5bdb92ab7bc9bc
17TheWord <17theword@gmail.com>
2023-11-20 20:05:11 +0800

    修正支持的适配器列表

--- nonebot_plugin_mcqq/__init__.py
+++ nonebot_plugin_mcqq/__init__.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from mcqq_tool.config import Config
 from mcqq_tool.permission import permission_check
-from mcqq_tool.utils import send_msg_to_mc, send_cmd_to_mc, send_send_title_to_mc
+from mcqq_tool.utils import send_msg_to_mc, send_cmd_to_mc, send_send_title_to_mc, send_actionbar_to_mc
 from nonebot import on_message, on_command, get_driver
 from nonebot.adapters import Message
 from nonebot.adapters.onebot.v11 import Bot as OneBot, GroupMessageEvent
@@ -24,7 +24,7 @@ __plugin_meta__ = PluginMetadata(
     config=Config,
     type="application",
     supported_adapters={
-        "nonebot.adapters.onebot.v11"
+        "nonebot.adapters.onebot.v11",
         "nonebot.adapters.qq"
     }
 )
@@ -34,6 +34,14 @@ mc_qq = on_message(priority=2, rule=msg_rule)
 mc_qq_send_title = on_command(
     "mc_qq_send_title",
     aliases={"mcst"},
+    priority=1,
+    rule=msg_rule,
+    block=True
+)
+
+mc_qq_actionbar = on_command(
+    "mc_qq_actionbar",
+    aliases={"mca"},
     priority=1,
     rule=msg_rule,
     block=True
@@ -92,3 +100,16 @@ async def handle_title(
     if arg := args.extract_plain_text():
         if back_msg := await send_send_title_to_mc(event=event, arg=arg):
             await mc_qq_send_title.finish(back_msg)
+
+
+@mc_qq_actionbar.handle()
+async def handle_actionbar(
+        matcher: Matcher,
+        bot: Union[OneBot, QQBot],
+        event: Union[GroupMessageEvent, GuildMessageEvent, MessageCreateEvent],
+        args: Message = CommandArg()
+):
+    await permission_check(matcher=matcher, bot=bot, event=event)
+    if arg := args.extract_plain_text():
+        if back_msg := await send_actionbar_to_mc(event=event, arg=arg):
+            await mc_qq_actionbar.finish(back_msg)
