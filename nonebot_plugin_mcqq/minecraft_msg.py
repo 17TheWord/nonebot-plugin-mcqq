1ca0b1f7de2aaac8ee21f778f95c8925c7b7c2f2
17TheWord <17theword@gmail.com>
2024-04-16 12:21:24 +0800

    🐛 move BaseDeathEvent to on_message()

--- nonebot_plugin_mcqq/minecraft_msg.py
+++ nonebot_plugin_mcqq/minecraft_msg.py
@@ -1,8 +1,7 @@
 from typing import Union
 
-from nonebot import on_message, on_notice, logger
+from nonebot import on_message, on_notice
 from nonebot.adapters.minecraft import BaseChatEvent, BaseJoinEvent, BaseQuitEvent, BaseDeathEvent
-from nonebot.exception import FinishedException
 
 from mcqq_tool.rule import mc_msg_rule
 from mcqq_tool.send_to_qq import send_mc_msg_to_qq
@@ -13,22 +12,12 @@ on_mc_notice = on_notice(priority=4, rule=mc_msg_rule)
 
 
 @on_mc_msg.handle()
-async def handle_mc_msg(event: BaseChatEvent):
-    msg_result = f"{event.player.nickname} 说：{event.message}"
+async def handle_mc_msg(event: Union[BaseChatEvent, BaseDeathEvent]):
+    msg_result = str(event.message) if isinstance(event, BaseDeathEvent) else f"{event.player.nickname} 说：{event.message}"
     await send_mc_msg_to_qq(event.server_name, msg_result)
 
 
 @on_mc_notice.handle()
-async def handle_mc_notice(event: Union[BaseJoinEvent, BaseQuitEvent, BaseDeathEvent]):
-    msg_result = ""
-    if isinstance(event, BaseJoinEvent):
-        msg_result += f"{event.player.nickname} 加入了游戏"
-    elif isinstance(event, BaseQuitEvent):
-        msg_result += f"{event.player.nickname} 退出了游戏"
-    elif isinstance(event, BaseDeathEvent):
-        msg_result += f"{event.player.nickname} 死了"
-    else:
-        logger.warning(f"[MC_QQ]丨未知的事件类型: {type(event)}，无法转发")
-        raise FinishedException
-
+async def handle_mc_notice(event: Union[BaseJoinEvent, BaseQuitEvent]):
+    msg_result = f"{event.player.nickname} {'加入' if isinstance(event, BaseJoinEvent) else '退出'}了游戏"
     await send_mc_msg_to_qq(event.server_name, msg_result)
