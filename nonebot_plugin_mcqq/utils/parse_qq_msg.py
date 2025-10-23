309da74e19d7b61471faf0fbd35f16923798f197
17TheWord <17theword@gmail.com>
2025-10-23 17:47:36 +0800

    ✨ 重构消息解析逻辑，优化 QQ 消息处理，支持回复消息引用

--- nonebot_plugin_mcqq/utils/parse_qq_msg.py
+++ nonebot_plugin_mcqq/utils/parse_qq_msg.py
@@ -1,4 +1,5 @@
-from nonebot.adapters.minecraft import Message, MessageSegment
+from nonebot.adapters.minecraft import Message as MCMessage
+from nonebot.adapters.minecraft import MessageSegment
 from nonebot.adapters.minecraft.models import (
     ClickAction,
     ClickEvent,
@@ -9,101 +10,18 @@ from nonebot.adapters.minecraft.models import (
 )
 from nonebot.adapters.onebot.v11 import Bot as OneBot
 from nonebot.adapters.onebot.v11 import GroupMessageEvent as OneBotGroupMessageEvent
+from nonebot.adapters.onebot.v11 import Message as OneBotMessage
 from nonebot.adapters.qq import Bot as QQBot
-from nonebot.adapters.qq import GroupAtMessageCreateEvent as QQGroupAtMessageCreateEvent
-from nonebot.adapters.qq import GuildMessageEvent as QQGuildMessageEvent
+from nonebot.adapters.qq import GroupAtMessageCreateEvent as QQGroupMessageEvent
+from nonebot.adapters.qq import (
+    GuildMessageEvent as QQGuildMessageEvent,
+)
+from nonebot.adapters.qq import (
+    Message as QQMessage,
+)
 
 from ..config import plugin_config
-
-# TODO 优化以下代码，添加过期机制
-
-ONEBOT_GROUP_MEMBER_NICKNAME_CACHE = {}
-"""缓存OneBot群成员昵称"""
-
-ONEBOT_GROUP_NAME_CACHE = {}
-"""缓存OneBot群名称"""
-
-QQ_GUILD_MEMBER_NICKNAME_CACHE = {}
-"""缓存QQ频道成员昵称"""
-
-QQ_GUILD_NAME_CACHE = {}
-"""缓存QQ频道名称"""
-
-QQ_CHANNEL_NAME_CACHE = {}
-"""缓存QQ子频道名称"""
-
-
-async def __get_group_or_nick_name(
-    bot: QQBot | OneBot,
-    event: QQGuildMessageEvent | QQGroupAtMessageCreateEvent | OneBotGroupMessageEvent,
-    user_id: str | None = None,
-) -> str:
-    """
-    获取群名或者成员昵称，如果user_id为空则获取群名
-    :param bot: 平台Bot实例
-    :param event: 事件
-    :param user_id: 用户ID
-    :return: 昵称
-    """
-    if isinstance(event, OneBotGroupMessageEvent) and isinstance(bot, OneBot):
-        if user_id:
-            if event.user_id == int(user_id):
-                return str(event.sender.card or event.sender.nickname)
-            elif not (
-                nickname := ONEBOT_GROUP_MEMBER_NICKNAME_CACHE.get(
-                    (event.group_id, user_id)
-                )
-            ):
-                nickname = (
-                    await bot.get_group_member_info(
-                        group_id=event.group_id, user_id=int(user_id), no_cache=True
-                    )
-                )["nickname"]
-                ONEBOT_GROUP_MEMBER_NICKNAME_CACHE[(event.group_id, user_id)] = nickname
-                return nickname
-        elif not (group_name := ONEBOT_GROUP_NAME_CACHE.get(event.group_id)):
-            group_name = (await bot.get_group_info(group_id=event.group_id))[
-                "group_name"
-            ]
-            ONEBOT_GROUP_NAME_CACHE[event.group_id] = group_name
-            return group_name
-
-    elif isinstance(event, QQGuildMessageEvent) and isinstance(bot, QQBot):
-        if user_id:
-            if event.author.id == user_id:
-                return str(
-                    (event.member and event.member.nick) or event.author.username
-                )
-            elif not (
-                member_name := QQ_GUILD_MEMBER_NICKNAME_CACHE.get(
-                    (event.guild_id, user_id)
-                )
-            ):
-                member = await bot.get_member(guild_id=event.guild_id, user_id=user_id)
-                member_name = member.nick or (member.user and member.user.username)
-                QQ_GUILD_MEMBER_NICKNAME_CACHE[(event.guild_id, user_id)] = member_name
-                return str(member_name)
-        else:
-            if not (guild_name := QQ_GUILD_NAME_CACHE.get(event.guild_id)):
-                guild = await bot.get_guild(guild_id=event.guild_id)
-                guild_name = guild.name
-                QQ_GUILD_NAME_CACHE[event.guild_id] = guild.name
-            if not (channel_name := QQ_CHANNEL_NAME_CACHE.get(event.channel_id)):
-                channel = await bot.get_channel(channel_id=event.channel_id)
-                channel_name = channel.name
-                QQ_CHANNEL_NAME_CACHE[event.channel_id] = channel.name
-
-            return f"[{guild_name}/{channel_name}]"
-
-    elif isinstance(event, QQGroupAtMessageCreateEvent) and isinstance(bot, QQBot):
-        # TODO 等待QQ机器人完善API，目前主动消息条数太少，无法测试
-        return event.author.member_openid if user_id else event.group_openid
-    return "未知名称" if user_id else "[未知群名]"
-
-
-def __normalize_url(url: str) -> str:
-    """标准化URL，确保以http开头"""
-    return url if url.startswith("http") else f"https://{url}"
+from .qq_util import get_group_or_nick_name, get_qq_channel_name, normalize_url
 
 
 def __create_hover_click_events(
@@ -123,84 +41,72 @@ def __create_hover_click_events(
     return hover_event, click_event
 
 
-async def __get_common_qq_msg_parsing(
+async def __parse_message_to_mc_message_segment(
+    message: QQMessage | OneBotMessage,
     bot: QQBot | OneBot,
-    event: QQGuildMessageEvent | QQGroupAtMessageCreateEvent | OneBotGroupMessageEvent,
-) -> tuple[Message, str]:
-    """
-    获取QQ消息解析后的消息列表和日志文本
-    :param bot: Bot对象
-    :param event: 事件对象
-    :param rcon_mode: 是否为RCON模式
-    :return: 消息列表和日志文本
-    """
+    event,
+    reply_mode: bool = False,
+) -> tuple[list[Component], str]:
     log_text = ""
-    message_list = Message()
 
-    for msg in event.get_message():
+    component_list: list[Component] = []
+    for msg in message:
         text = ""
-        color = None
-        hover_event = None
-        click_event = None
-        # 是否直接添加文本（不包装MessageSegment）
-        raw_append = False
+        color: Color | None = None
+        hover_event: HoverEvent | None = None
+        click_event: ClickEvent | None = None
+        ci_code = ""
 
         # 处理文本消息
         if msg.type == "text":
             text = str(msg.data["text"]).replace("\r", "").replace("\n", "\n * ")
-            raw_append = True
 
         # 处理图片/附件
         elif msg.type in ["image", "attachment"]:
-            url = __normalize_url(msg.data["url"])
+            text = "[图片]"
+            url = normalize_url(msg.data["url"])
             ci_code = f"[[CICode,url={url}]]"
 
-            if plugin_config.chat_image_enable:
-                text = ci_code
-                raw_append = True
-            else:
-                text = "[图片]"
-                color = Color.light_purple
-                hover_event = HoverEvent(
-                    action=HoverAction.show_text,
-                    contents=[Component(text=ci_code, color=Color.dark_purple)],
-                )
+            color = Color.light_purple
+            hover_event, click_event = __create_hover_click_events(url, "点击查看图片")
 
         # 处理视频
         elif msg.type == "video":
             text = "[视频]"
             color = Color.light_purple
-            url = __normalize_url(msg.data["url"])
-            hover_event, click_event = __create_hover_click_events(url, text)
+            url = normalize_url(msg.data["url"])
+            hover_event, click_event = __create_hover_click_events(url, "点击查看视频")
 
         # 处理分享
         elif msg.type == "share":
             text = "[分享]"
             color = Color.gold
-            url = __normalize_url(msg.data["url"])
-            hover_event, click_event = __create_hover_click_events(url, text)
+            url = normalize_url(msg.data["url"])
+            hover_event, click_event = __create_hover_click_events(url, "点击查看分享")
 
         # 处理@消息（OneBot）
         elif msg.type == "at":
             if msg.data["qq"] == "all":
                 text = "@全体成员"
             else:
-                nickname = await __get_group_or_nick_name(bot, event, msg.data["qq"])
-                text = f"@{nickname}"
+                text = f"@{await get_group_or_nick_name(bot, event, msg.data['qq'])}"
             color = Color.green
 
         # 处理@用户（QQ）
         elif msg.type == "mention_user":
-            nickname = await __get_group_or_nick_name(bot, event, msg.data["user_id"])
-            text = f"@{nickname}"
+            if reply_mode:
+                text = "某用户"
+            else:
+                text = await get_group_or_nick_name(bot, event, msg.data["user_id"])
             color = Color.green
 
         # 处理@子频道（QQ）
-        elif msg.type == "mention_channel" and isinstance(event, QQGuildMessageEvent):
-            if not (channel_name := QQ_CHANNEL_NAME_CACHE.get(event.channel_id)):
-                channel = await bot.get_channel(channel_id=event.channel_id)
-                channel_name = channel.name
-                QQ_CHANNEL_NAME_CACHE[event.channel_id] = channel_name
+        elif (
+            msg.type == "mention_channel"
+            and isinstance(event, QQGuildMessageEvent)
+            and isinstance(bot, QQBot)
+        ):
+            channel_name = await get_qq_channel_name(bot, event.channel_id)
             text = f"@{channel_name}"
             color = Color.green
 
@@ -221,32 +127,63 @@ async def __get_common_qq_msg_parsing(
 
         # 未知类型
         else:
-            text = "[未知消息类型]"
+            text = f"[未知消息类型 {msg.type}]"
 
         # 统一处理文本和添加到消息列表
-        display_text = text.strip() + " "
-        log_text += display_text
-
-        if raw_append:
-            message_list.append(text + " ")
+        log_text += text.strip() + " "
+        if ci_code and plugin_config.chat_image_enable and not reply_mode:
+            text = ci_code
+        if reply_mode:
+            component_list.append(Component(text=text, color=color))
         else:
-            message_list.append(
-                MessageSegment.text(
-                    text=display_text,
+            component_list.append(
+                Component(
+                    text=text,
                     color=color,
-                    hover_event=hover_event,
-                    click_event=click_event,
+                    hoverEvent=hover_event,
+                    clickEvent=click_event,
                 )
             )
 
-    return message_list, log_text
+    return component_list, log_text
+
+
+async def __process_reply_message(
+    reply_message: OneBotMessage | QQMessage,
+    reply_author_name: str,
+    bot: OneBot | QQBot,
+    event,
+) -> tuple[MessageSegment, str]:
+    """
+    处理回复消息，生成 MessageSegment 和日志文本
+    :param reply_message: 回复的消息内容
+    :param reply_author_name: 回复消息作者的昵称
+    :param bot: Bot实例
+    :param event: 事件
+    :return: MessageSegment 和日志文本
+    """
+    reply_mc_message, msg_log_text = await __parse_message_to_mc_message_segment(
+        message=reply_message, bot=bot, event=event, reply_mode=True
+    )
+    reply_template = f" 回复 @{reply_author_name} 的消息 "
+
+    message_segment = MessageSegment.text(
+        text=reply_template,
+        color=Color.gray,
+        hover_event=HoverEvent(
+            action=HoverAction.show_text,
+            contents=reply_mc_message,
+        ),
+    )
+
+    log_text = reply_template + msg_log_text
+    return message_segment, log_text
 
 
 async def parse_qq_msg_to_component(
     bot: QQBot | OneBot,
-    event: QQGuildMessageEvent | QQGroupAtMessageCreateEvent | OneBotGroupMessageEvent,
-    rcon_mode: bool = False,
-) -> tuple[Message, str]:
+    event: QQGroupMessageEvent | QQGuildMessageEvent | OneBotGroupMessageEvent,
+) -> tuple[MCMessage, str]:
     """
     解析 QQ 消息，转为 WebSocketBody 模型
     :param bot: 聊天平台Bot实例
@@ -254,17 +191,17 @@ async def parse_qq_msg_to_component(
     :return: Message
     """
 
-    message_list = Message()
+    message_list = MCMessage()
     log_text = ""
 
     # 群聊名称
     if plugin_config.send_group_name:
-        group_name = f"{await __get_group_or_nick_name(bot, event)} "
+        group_name = f"{await get_group_or_nick_name(bot, event)} "
         message_list.append(MessageSegment.text(text=group_name, color=Color.aqua))
         log_text += group_name
 
     # 消息发送者昵称
-    sender_nickname_text = await __get_group_or_nick_name(
+    sender_nickname_text = await get_group_or_nick_name(
         bot, event, str(event.get_user_id())
     )
     message_list.append(
@@ -272,29 +209,50 @@ async def parse_qq_msg_to_component(
     )
     log_text += sender_nickname_text
 
+    # 回复消息（OneBot）
+    if (
+        isinstance(event, OneBotGroupMessageEvent)
+        and isinstance(bot, OneBot)
+        and (reply := event.reply)
+    ):
+        reply_author_name = str(
+            reply.sender.card or reply.sender.nickname or "未知名称"
+        )
+        reply_segment, reply_log_text = await __process_reply_message(
+            reply_message=reply.message,
+            reply_author_name=reply_author_name,
+            bot=bot,
+            event=event,
+        )
+        message_list.append(reply_segment)
+        log_text += reply_log_text
+    # 回复消息（QQ）
+    elif (
+        isinstance(event, QQGuildMessageEvent)
+        and isinstance(bot, QQBot)
+        and (reply := event.reply)
+    ):
+        reply_author_name = str(reply.author.username or "未知名称")
+        reply_segment, reply_log_text = await __process_reply_message(
+            reply_message=QQMessage.from_guild_message(reply),
+            reply_author_name=reply_author_name,
+            bot=bot,
+            event=event,
+        )
+        message_list.append(reply_segment)
+        log_text += reply_log_text
+
+    component_list, msg_log_text = await __parse_message_to_mc_message_segment(
+        message=event.get_message(), bot=bot, event=event
+    )
+
     # 消息 '说：'
     message_list.append(
-        MessageSegment.text(text=plugin_config.say_way, color=Color.white)
+        MessageSegment.text(
+            text=plugin_config.say_way, color=Color.white, extra=component_list
+        )
     )
     log_text += f" {plugin_config.say_way}"
-
-    # 消息内容
-    temp_message_list, msg_log_text = await __get_common_qq_msg_parsing(bot, event)
     log_text += msg_log_text
 
-    message_list.append(*temp_message_list)
-
     return message_list, log_text
-
-
-def parse_qq_screen_cmd_to_rcon_model(command_type: str, command: str):
-    """
-    解析 QQ 消息，转为 Rcon命令 模型
-    :param command_type: 命令类型
-    :param command: 命令内容
-    :return: 命令文本
-    """
-    if command_type == "action_bar":
-        return f'title @a actionbar "{command}"'
-    else:
-        return f'title @a "{command}"'
